a3df4c5b7c542542e29a92c72224caf5f125266a
Emma Jellema-Butler <emma.jellemabutler@gmail.com>
2023-11-15 14:20:22 +0200

    small fix

--- app/api/[post]/route.ts
+++ app/api/[post]/route.ts
@@ -2,17 +2,6 @@ import { readFileSync } from "fs"
 import matter from "gray-matter"
 import path from "path"
 
-export function getPost(slug: string) {
-    const markdownFile = readFileSync(path.join('_posts',`${slug}.mdx`), 'utf-8')
-
-    const { data: frontMatter, content } = matter(markdownFile)
-
-    return {
-        frontMatter,
-        slug,
-        content
-    }
-}
 
 export async function GET(request: Request,  { params }: { params: { slug: string } }) {
     const slug = params.slug;
