081df1cf177016a7089c8603cca2e27fff1abe8c
Emma Jellema-Butler <emma.jellemabutler@gmail.com>
2023-11-16 11:28:24 +0200

    added a little icon

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -12,7 +12,7 @@ export default function ContactPage() {
     return (
         <article className="px-4 py-4">
             <div className="w-1/2 mx-auto divide-y-2 divide-light-800 border-2 border-light-700 rounded">
-                <ContactCard href="https://www.emmalexandria.dev">Portfolio</ContactCard>
+                <ContactCard href="https://www.emmalexandria.dev" icon="mdi:briefcase">Portfolio</ContactCard>
                 <ContactCard href="https://www.github.com/emmalexandria" icon="mdi:github">Github</ContactCard>
                 <ContactCard href="https://www.linkedin.com/in/emma-jb" icon="mdi:linkedin">LinkedIn</ContactCard>
             </div>
