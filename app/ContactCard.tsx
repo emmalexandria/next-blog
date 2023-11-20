c99d9329bf93ab0fb7e1142593f4fde5566d6030
Emma Jellema-Butler <emma.jellemabutler@gmail.com>
2023-11-20 18:53:26 +0200

    fix the contact section

--- app/ContactCard.tsx
+++ app/ContactCard.tsx
@@ -3,9 +3,12 @@ import { Icon } from "@iconify/react/dist/iconify.js";
 
 export default function ContactCard(props: {href: string, icon?:string, children: React.ReactNode}) {
     return (
-        <div className="p-2 md:p-4 space-x-2 flex flex-row items-center text-dark-100 hover:text-accent-500">
-            {props.icon && <Icon icon={props.icon} height={36}/>}
-            <a className="font-body text-xl" href={props.href} target="_blank">{props.children}</a>
-        </div>
+        <a href={props.href} target="_blank" className="py-1 md:py-2 block text-xl hover:text-accent-500 ">
+<           div className="space-x-2 flex flex-row items-center">
+                {props.icon && <Icon icon={props.icon}/>}
+                <p className="font-body" >{props.children}</p>
+            </div>
+        </a>
+        
     )
 }
